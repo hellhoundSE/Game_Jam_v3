d15074e1cf8e9658e895d2457a84245462dc7bea
Fedir Studinskyi <s19428@pjwstk.edu.pl>
2020-11-01 01:33:51 +0200

    camera script added, minor changes

---
+++ Assets/CameraScript.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraScript : MonoBehaviour
+{
+    private GameObject player;
+
+    public Vector2 maxDeviationFromStartingPoint;
+    public float followingSpeed_lerp;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        player = GameObject.FindGameObjectWithTag("Player");
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        Vector2 newPosition = 
+            Vector2.Lerp(
+                new Vector2(transform.position.x, transform.position.y), 
+                new Vector2(player.transform.position.x, player.transform.position.y),
+                followingSpeed_lerp);
+        transform.position = 
+            new Vector3(
+                Mathf.Clamp(newPosition.x, -maxDeviationFromStartingPoint.x, maxDeviationFromStartingPoint.x),
+                Mathf.Clamp(newPosition.y, -maxDeviationFromStartingPoint.y, maxDeviationFromStartingPoint.y),
+                transform.position.z);
+    }
+}
--- Assets/Scenes/FedorTestScene/Prefabs/Enemy/EnemyMovement.cs
+++ Assets/Scenes/FedorTestScene/Prefabs/Enemy/EnemyMovement.cs
@@ -4,23 +4,31 @@ using UnityEngine;
 
 public class EnemyMovement : MonoBehaviour
 {
-    public GameObject player;
+    public bool stayOnPlatform;
+    public bool jumpWithAnInterval = true;
+
+    private GameObject player;
     public float distanceThreshold;
     private float distanceToPlayer;
 
+    public Transform topRay;
     public Transform leftRay;
     public Transform middlRay;
     public Transform rightRay;
     public float isGroundedThreshold = 1.5f;
+    public float isUnderPlatformTheshold = 2;
 
     public GameObject hitObject;
     private float distanceToHit;
 
     private Rigidbody2D rigidbody;
+    public const float jumpTimer = 2;
+    public float jumpTimer_; 
+    public float jumpForce;
     public float normalSpeed;
     public float followingSpeed;
 
-    public int decreaseHealthBy = 1;
+    public int decreaseHealthBy = 1; // health taken from player on collision
 
     // Start is called before the first frame update
     void Start()
@@ -34,10 +42,13 @@ public class EnemyMovement : MonoBehaviour
     {
         distanceToPlayer = Vector3.Distance(transform.position, player.transform.position);
 
-        GroundRaycast();
+        //GroundRaycast();
 
         if (distanceToPlayer < distanceThreshold)
             FollowPlayer();
+
+
+        EnemyJump();
     }
 
     bool IsRayOnGround(Transform rayTransform)
@@ -45,16 +56,48 @@ public class EnemyMovement : MonoBehaviour
         return GetHitDistance(rayTransform) < isGroundedThreshold;
     }
 
-    void GroundRaycast()
+
+    void EnemyJump()
     {
-        GetHitDistance(leftRay);
-        GetHitDistance(middlRay);
-        GetHitDistance(rightRay);
+        if (!jumpWithAnInterval)
+            return;
+
+        if (jumpTimer_ <= 0)
+        {
+            float d = GetHitDistance(topRay, Vector2.up);
+            if (d == -1)
+                d = Mathf.Infinity;
+            Debug.Log(d);
+            if (d > isUnderPlatformTheshold)
+            {
+                ///
+                rigidbody.AddForce(new Vector2(0, jumpForce), ForceMode2D.Impulse);
+                ///
+            }
+
+            jumpTimer_ = jumpTimer;
+
+            return;
+        }
+        else
+        {
+            jumpTimer_ -= Time.deltaTime;
+        }
     }
 
-    float GetHitDistance(Transform origin)
+    //void GroundRaycast()
+    //{
+    //    GetHitDistance(leftRay);
+    //    GetHitDistance(middlRay);
+    //    GetHitDistance(rightRay);
+    //}
+
+    float GetHitDistance(Transform origin, Vector2? direction = null)
     {
-        RaycastHit2D hit = Physics2D.Raycast(origin.position, -Vector2.up);
+        if (direction == null)
+            direction = -Vector2.up;
+
+        RaycastHit2D hit = Physics2D.Raycast(origin.position, (Vector2)direction);
 
         if (hit.collider != null)
         {
@@ -70,14 +113,14 @@ public class EnemyMovement : MonoBehaviour
     {
         if (player.transform.position.x < transform.position.x)
         {
-            if (IsRayOnGround(leftRay))
+            if (IsRayOnGround(leftRay) || !stayOnPlatform)
             {
                 MovePlayerByX(-followingSpeed * Time.deltaTime);
             }
         }
         if (player.transform.position.x > transform.position.x)
         {
-            if (IsRayOnGround(rightRay))
+            if (IsRayOnGround(rightRay) || !stayOnPlatform)
             {
                 MovePlayerByX(followingSpeed * Time.deltaTime);
             }
