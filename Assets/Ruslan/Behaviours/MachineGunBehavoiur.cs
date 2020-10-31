96030645665cc5b6bd88c32007365da2965eff5d
valuievruslan <valuievruslan@gmail.com>
2020-10-31 17:05:59 +0100

    Item system

---
+++ Assets/Ruslan/Behaviours/MachineGunBehavoiur.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MachineGunBehavoiur : GunBehaviour
+{
+
+    public override void MakeShoot()
+    {
+        GameObject theBullet = Instantiate(Bullet, transform.position, Quaternion.identity) as GameObject;
+        Rigidbody2D bulletRigidBody = theBullet.GetComponent<Rigidbody2D>();
+        bulletRigidBody.AddForce(Vector3.forward * Speed);
+    }
+
+}
---
+++ Assets/Ruslan/Shooting.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Shooting : MonoBehaviour
+{
+    public Gun Gun;
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.F))
+        {
+            Gun.Shoot();
+        }   
+    }
+}
